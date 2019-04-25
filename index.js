8d04988b751e34cb416d9ec1c508a9a9542377b7
Junior <juniorcorol@hotmail.com>
2019-04-25 01:29:29 -0300

    Redirecionando / para index;html

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ var fs = require('fs'),
     http = require('http');
 
 http.createServer(function (req, res) {
-  fs.readFile(__dirname + req.url, function (err,data) {
-    if (err) {
-        console.log(err)
-      res.writeHead(404);
-      res.end(JSON.stringify(err));
-      return;
+    function render(err, data) {
+        console.log(req.url)
+
+        if (err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.writeHead(200);
+        res.end(data);
     }
-    res.writeHead(200);
-    res.end(data);
-  });
+    if (req.url === '/') {
+        fs.readFile(__dirname + '/index.html', render);
+    } else {
+        fs.readFile(__dirname + req.url, render);
+    }
 }).listen(process.env.PORT || 8080);
